da79a95a356841df7249129174befe13bfc3f7a6
CROW77 <mairamillerdonega@gmail.com>
2024-11-26 18:07:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(700, 500);
+  background(77);
+  }
+
+function draw() {  
+  
+ stroke ("gray");
+  fill ("gray");
+  
+  //console.Log(mouseIsPressed);
+  
+  if (mouseIsPressed)  { circle(mouseX,mouseY,30,30);
+                        
+    }
+}
